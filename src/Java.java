71337ec0593b7a29fc8be57f4f0dffa316f34ec8
habibazaraei <habibazaraei24@gmail.com>
2024-02-03 20:58:10 -0500

    updated file name

---
+++ src/Java.java
@@ -0,0 +1,33 @@
+public class Java {
+    public static void main(String[] args) {
+
+        /* Declaring Variables */
+        int age = 25;
+        double salary = 50000.0;
+        String name = "John";
+
+        /* For Loop */
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Iteration: " + i);
+        }
+
+        /* While Loop */
+        int count = 0;
+        while (count < 3) {
+            System.out.println("Count: " + count);
+            count++;
+        }
+
+        /* Do-While Loop */
+        int num = 1;
+        do {
+            System.out.println("Number : " + num);
+            num++;
+        } while (num <+3);
+
+
+
+
+
+        }
+    }
