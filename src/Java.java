bddee58bf712431c4ba68be6d47609b6026c0e12
habibazaraei <habibazaraei24@gmail.com>
2024-02-03 21:14:22 -0500

    Completed Homework 1

--- src/Java.java
+++ src/Java.java
@@ -23,11 +23,48 @@ public class Java {
         do {
             System.out.println("Number : " + num);
             num++;
-        } while (num <+3);
+        } while (num < +3);
 
-
-
-
-
+        /* If Statments */
+        int x = 10;
+        if (x > 5) {
+            System.out.println("x is greater than 5");
         }
+
+        /* If-Else Statments */
+        int number = 7;
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+
+        /* Nested If-Else */
+        int ages = 18;
+        if (ages >= 18) {
+            if (ages < 21) {
+                System.out.println("You're eligible but not for alcohol");
+            } else {
+                System.out.println("You're eligible for everything");
+            }
+        } else {
+            System.out.println("You're not eligible");
+        }
+
+        /* Switch Case */
+        int day = 3;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            // ...
+            default:
+                System.out.println("Invalid day");
+        }
+
+
     }
+}
